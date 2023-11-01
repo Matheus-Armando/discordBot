6e0aac956d79841b4e0286c82a3486c6be2dda2b
Felipe Maccari <felipecesarmaccari@gmail.com>
2023-11-01 15:44:57 -0300

    feat: add prop validation on player puuid route

--- src/routes/player.ts
+++ src/routes/player.ts
@@ -3,8 +3,16 @@ import { type FastifyInstance, type FastifyRequest } from 'fastify'
 import { getApi } from '../service'
 
 export const playerRoutes = async (server: FastifyInstance): Promise<any> => {
-  server.get('/player/', async function handler (request: FastifyRequest, reply) {
-    const { gameName, tagLine } = request.query as { gameName: string, tagLine: string }
+  server.get('/player/puuid', async function handler (request: FastifyRequest, reply) {
+    const { gameName, tagLine } = request.query as { gameName: any, tagLine: any }
+
+    if (!gameName) {
+      return await reply.code(400).send({ error: 'The gameName property is invalid' })
+    }
+
+    if (!tagLine) {
+      return await reply.code(400).send({ error: 'The tagLine property is invalid' })
+    }
 
     const api = await getApi()
 
