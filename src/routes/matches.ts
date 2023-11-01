df3de335d13f617a99a80f1a8c1b080d4d944150
Felipe Maccari <felipecesarmaccari@gmail.com>
2023-11-01 18:44:03 -0300

    feat: add route to resume match

--- src/routes/matches.ts
+++ src/routes/matches.ts
@@ -26,7 +26,7 @@ export const matchesRoutes = async (server: FastifyInstance): Promise<any> => {
     return playerMatches.data
   })
 
-  server.get('/by-uuid/players', async function handler (request: FastifyRequest, reply) {
+  server.get('/by-puuid/matches/resume', async function handler (request: FastifyRequest, reply) {
     const { puuid, numberMatches } = request.query as ByPuuid
     let apiUrl = `${process.env.API_BASE_URL}/${ROUTE_PATH}/by-puuid/${puuid}/ids`
 
@@ -47,11 +47,57 @@ export const matchesRoutes = async (server: FastifyInstance): Promise<any> => {
 
     const matchesInformationPromises = await playerMatches.data.map(async (matchId: string) => {
       const matchInformation = await api.get(`${process.env.API_BASE_URL}/${ROUTE_PATH}/${matchId}`)
-      return matchInformation.data.info.participants
+      return matchInformation.data
     })
 
-    const matchesInformation = await Promise.all(matchesInformationPromises)
+    const participantMatchesInformation = await Promise.all(matchesInformationPromises)
 
-    return matchesInformation
+    let win = 0
+    let lose = 0
+
+    participantMatchesInformation.forEach((match) => {
+      const actualPlayer = match.info.participants.find((participant: any) => {
+        return participant.puuid === puuid
+      })
+
+      const playerTeam = match.info.teams.find((team: any) => team.teamId === actualPlayer.teamId)
+
+      if (playerTeam.win) {
+        win++
+      } else {
+        lose++
+      }
+    })
+
+    return { win, lose }
+  })
+
+  server.get('/by-puuid/players', async function handler (request: FastifyRequest, reply) {
+    const { puuid, numberMatches } = request.query as ByPuuid
+    let apiUrl = `${process.env.API_BASE_URL}/${ROUTE_PATH}/by-puuid/${puuid}/ids`
+
+    if (!puuid) {
+      return await reply.code(400).send({ error: 'The gameName property is required' })
+    }
+
+    if (numberMatches) {
+      apiUrl = `${apiUrl}?count=${numberMatches}`
+    }
+
+    const api = await getApi()
+    const playerMatches = await api.get(apiUrl)
+
+    if (playerMatches.length === 0) {
+      return await reply.code(400).send({ error: 'This player has no matches' })
+    }
+
+    const matchesInformationPromises = await playerMatches.data.map(async (matchId: string) => {
+      const matchInformation = await api.get(`${process.env.API_BASE_URL}/${ROUTE_PATH}/${matchId}`)
+      return matchInformation.data.metadata
+    })
+
+    const participantMatchesInformation = await Promise.all(matchesInformationPromises)
+
+    return participantMatchesInformation
   })
 }
