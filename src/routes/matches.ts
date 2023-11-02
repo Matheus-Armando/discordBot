00a5dc80e34274a08b327d81a5510bbd3e0e2738
Felipe Maccari <felipecesarmaccari@gmail.com>
2023-11-02 10:15:05 -0300

    feat: add avh to kda

--- src/routes/matches.ts
+++ src/routes/matches.ts
@@ -2,14 +2,14 @@ import { type FastifyInstance, type FastifyRequest } from 'fastify'
 
 import { getApi } from '../service'
 
-const ROUTE_PATH = 'lol/match/v5/matches'
+const ROUTE_PATH = `${process.env.API_BASE_URL}/lol/match/v5/matches`
 
 interface ByPuuid { puuid: any, numberMatches: any }
 
 export const matchesRoutes = async (server: FastifyInstance): Promise<any> => {
   server.get('/by-puuid', async function handler (request: FastifyRequest, reply) {
     const { puuid, numberMatches } = request.query as ByPuuid
-    let apiUrl = `${process.env.API_BASE_URL}/${ROUTE_PATH}/by-puuid/${puuid}/ids`
+    let apiUrl = `${ROUTE_PATH}/by-puuid/${puuid}/ids`
 
     if (!puuid) {
       return await reply.code(400).send({ error: 'The gameName property is required' })
@@ -28,7 +28,7 @@ export const matchesRoutes = async (server: FastifyInstance): Promise<any> => {
 
   server.get('/by-puuid/matches/resume', async function handler (request: FastifyRequest, reply) {
     const { puuid, numberMatches } = request.query as ByPuuid
-    let apiUrl = `${process.env.API_BASE_URL}/${ROUTE_PATH}/by-puuid/${puuid}/ids`
+    let apiUrl = `${ROUTE_PATH}/by-puuid/${puuid}/ids`
 
     if (!puuid) {
       return await reply.code(400).send({ error: 'The gameName property is required' })
@@ -41,12 +41,12 @@ export const matchesRoutes = async (server: FastifyInstance): Promise<any> => {
     const api = await getApi()
     const playerMatches = await api.get(apiUrl)
 
-    if (playerMatches.length === 0) {
+    if (playerMatches.data.length === 0) {
       return await reply.code(400).send({ error: 'This player has no matches' })
     }
 
     const matchesInformationPromises = await playerMatches.data.map(async (matchId: string) => {
-      const matchInformation = await api.get(`${process.env.API_BASE_URL}/${ROUTE_PATH}/${matchId}`)
+      const matchInformation = await api.get(`${ROUTE_PATH}/${matchId}`)
       return matchInformation.data
     })
 
@@ -54,11 +54,18 @@ export const matchesRoutes = async (server: FastifyInstance): Promise<any> => {
 
     let win = 0
     let lose = 0
+    let deaths = 0
+    let kills = 0
+    let assists = 0
 
     participantMatchesInformation.forEach((match) => {
       const actualPlayer = match.info.participants.find((participant: any) => {
         return participant.puuid === puuid
       })
+
+      deaths = deaths + actualPlayer.deaths
+      kills = kills + actualPlayer.kills
+      assists = assists + actualPlayer.assists
 
       const playerTeam = match.info.teams.find((team: any) => team.teamId === actualPlayer.teamId)
 
@@ -69,12 +76,16 @@ export const matchesRoutes = async (server: FastifyInstance): Promise<any> => {
       }
     })
 
-    return { win, lose }
+    const deathAvg = deaths / playerMatches.data.length
+    const killAvg = kills / playerMatches.data.length
+    const assistAvg = assists / playerMatches.data.length
+
+    return { win, lose, deathAvg, killAvg, assistAvg }
   })
 
   server.get('/by-puuid/players', async function handler (request: FastifyRequest, reply) {
     const { puuid, numberMatches } = request.query as ByPuuid
-    let apiUrl = `${process.env.API_BASE_URL}/${ROUTE_PATH}/by-puuid/${puuid}/ids`
+    let apiUrl = `${ROUTE_PATH}/by-puuid/${puuid}/ids`
 
     if (!puuid) {
       return await reply.code(400).send({ error: 'The gameName property is required' })
@@ -87,12 +98,12 @@ export const matchesRoutes = async (server: FastifyInstance): Promise<any> => {
     const api = await getApi()
     const playerMatches = await api.get(apiUrl)
 
-    if (playerMatches.length === 0) {
+    if (playerMatches.data.length === 0) {
       return await reply.code(400).send({ error: 'This player has no matches' })
     }
 
     const matchesInformationPromises = await playerMatches.data.map(async (matchId: string) => {
-      const matchInformation = await api.get(`${process.env.API_BASE_URL}/${ROUTE_PATH}/${matchId}`)
+      const matchInformation = await api.get(`${ROUTE_PATH}/${matchId}`)
       return matchInformation.data.metadata
     })
 
