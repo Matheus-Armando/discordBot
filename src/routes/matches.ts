90eec812360f3369ca1b8cdb87d28c9562c1f269
Felipe Maccari <felipecesarmaccari@gmail.com>
2023-11-02 22:02:12 -0300

    feat: add route to show stats from player teams

--- src/routes/matches.ts
+++ src/routes/matches.ts
@@ -5,6 +5,16 @@ import { getApi } from '../service'
 const ROUTE_PATH = `${process.env.API_BASE_URL}/lol/match/v5/matches`
 
 interface ByPuuid { puuid: any, numberMatches: any }
+interface Participant {
+  teamId: number
+  puuid: string
+}
+
+interface RelatedPlayers {
+  allies: string[]
+  enemies: string[]
+  players: string[]
+}
 
 export const matchesRoutes = async (server: FastifyInstance): Promise<any> => {
   server.get('/by-puuid', async function handler (request: FastifyRequest, reply) {
@@ -23,7 +33,86 @@ export const matchesRoutes = async (server: FastifyInstance): Promise<any> => {
 
     const playerMatches = await api.get(apiUrl)
 
+    if (playerMatches.data.length === 0) {
+      return await reply.code(400).send({ error: 'This player has no matches' })
+    }
+
     return playerMatches.data
+  })
+
+  server.get('/related-players', async function handler (request: FastifyRequest, reply) {
+    const { puuid, numberMatches } = request.query as ByPuuid
+    let apiUrl = `${ROUTE_PATH}/by-puuid/${puuid}/ids`
+
+    if (!puuid) {
+      return await reply.code(400).send({ error: 'The gameName property is required' })
+    }
+
+    if (numberMatches) {
+      apiUrl = `${apiUrl}?count=${numberMatches}`
+    }
+
+    const api = await getApi()
+
+    const playerMatches = await api.get(apiUrl)
+
+    if (playerMatches.data.length === 0) {
+      return await reply.code(400).send({ error: 'This player has no matches' })
+    }
+
+    const matchesInformationPromises = await playerMatches.data.map(async (matchId: string) => {
+      const matchInformation = await api.get(`${ROUTE_PATH}/${matchId}`)
+      return matchInformation.data
+    })
+
+    const participantMatchesInformation = await Promise.all(matchesInformationPromises)
+
+    const relatedPlayers: RelatedPlayers = { players: [], allies: [], enemies: [] }
+
+    participantMatchesInformation.forEach((match) => {
+      const { info } = match
+
+      const actualPlayer = match.info.participants.find((participant: any) => {
+        return participant.puuid === puuid
+      })
+
+      info.participants.forEach((participant: Participant) => {
+        if (participant.puuid !== puuid) {
+          if (participant.teamId === actualPlayer.teamId) {
+            relatedPlayers.allies.push(participant.puuid)
+          } else {
+            relatedPlayers.enemies.push(participant.puuid)
+          }
+
+          relatedPlayers.players.push(participant.puuid)
+        }
+      })
+    })
+
+    const playersSet = new Set(relatedPlayers.players)
+
+    const results = Array.from(playersSet).map((player) => {
+      let countEnemy = 0
+      let countAlly = 0
+
+      for (const innerPlayer of relatedPlayers.enemies) {
+        if (innerPlayer === player) {
+          countEnemy++
+        }
+      }
+
+      for (const inner of relatedPlayers.allies) {
+        if (inner === player) {
+          countAlly++
+        }
+      }
+
+      const count = countEnemy + countAlly
+
+      return { player, count, countAlly, countEnemy }
+    })
+
+    return results
   })
 
   server.get('/by-puuid/matches/resume', async function handler (request: FastifyRequest, reply) {
